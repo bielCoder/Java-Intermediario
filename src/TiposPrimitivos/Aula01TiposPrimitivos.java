158beb4bbd760bbab5f81d73e77e92a8e7111bd0
gdsousa <gdsousa@recordbrasilia.com.br>
2025-05-16 11:08:07 -0300

    Feat: tipos primitivos

---
+++ src/TiposPrimitivos/Aula01TiposPrimitivos.java
@@ -0,0 +1,26 @@
+package TiposPrimitivos;
+
+public class Aula01TiposPrimitivos {
+    public static void main(String[] args) {
+        /*
+        * My First Program
+        * */
+
+        /*
+        *  Tipos de valores a serem inseridos na memória
+        *  int , double, float, char, byte, short, long boolean
+        * */
+
+        int age = 28;
+        long bitinteger = (int) 100000L; // casting
+        double salary = 5500D;
+        float  height = 1.78F;
+        char gender = 'M';
+        byte birthday = 'M';
+        char caractere = '\u0041';
+        boolean working = true;
+
+        System.out.println("A idade é " + age + " anos");
+        System.out.println("char: " + caractere);
+    }
+}
